197bd4083824f79b17308b49c7c64c966fef3eb9
Anthony Nguyen <anthony@Anthonys-iMac.home>
2020-08-29 22:23:09 -0500

    added some cool stuffs

--- chrysalis-ui/src/Player.js
+++ chrysalis-ui/src/Player.js
@@ -2,6 +2,9 @@ import React, { Fragment, useState, useRef, useEffect } from 'react';
 import ReactPlayer from 'react-player';
 import Duration from './Duration';
 import khalid_better from './assets/khalid_better.mp3';
+import { makeStyles } from '@material-ui/core/styles';
+import {Button, Grid, Container} from '@material-ui/core';
+import './index.css';
 
 function Player() {
 
@@ -45,95 +48,102 @@ function Player() {
   const inputEl = useRef(null);
 
   return (
-    <Fragment>
-      <ReactPlayer
-        ref={inputEl}
-        className='react-player'
-        width='100%'
-        height='100%'
-        url={khalid_better}
-        playing={playing}
-        loop={loop}
-        volume={volume}
-        muted={muted}
-        onReady={() => console.log('onReady')}
-        onStart={() => console.log('onStart')}
-        onPlay={handlePlay}
-        onPause={handlePause}
-        onBuffer={() => console.log('onBuffer')}
-        onSeek={e => console.log('onSeek', e)}
-        onEnded={handleEnded}
-        onError={e => console.log('onError', e)}
-        onProgress={handleProgress}
-        onDuration={handleDuration}
-      />
+    <Container>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <ReactPlayer
+          ref={inputEl}
+          className='react-player'
+          width='100%'
+          height='100%'
+          url={khalid_better}
+          playing={playing}
+          loop={loop}
+          volume={volume}
+          muted={muted}
+          onReady={() => console.log('onReady')}
+          onStart={() => console.log('onStart')}
+          onPlay={handlePlay}
+          onPause={handlePause}
+          onBuffer={() => console.log('onBuffer')}
+          onSeek={e => console.log('onSeek', e)}
+          onEnded={handleEnded}
+          onError={e => console.log('onError', e)}
+          onProgress={handleProgress}
+          onDuration={handleDuration}
+        />
 
-      <table>
-        <tbody>
-          <tr>
-            <th>Controls</th>
-            <td>
-              <button onClick={handleStop}>Stop</button>
-              <button onClick={handlePlayPause}>{playing ? 'Pause' : 'Play'}</button>
-            </td>
-          </tr>
-          <tr>
-            <th>Seek</th>
-            <td>
-              <input
-                type='range' min={0} max={0.999999} step='any'
-                value={played}
-                onMouseDown={handleSeekMouseDown}
-                onChange={handleSeekChange}
-                onMouseUp={handleSeekMouseUp}
-              />
-            </td>
-          </tr>
-          <tr>
-            <th>Volume</th>
-            <td>
-              <input type='range' min={0} max={1} step='any' value={volume} onChange={handleVolumeChange} />
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <label htmlFor='muted'>Muted</label>
-            </th>
-            <td>
-              <input id='muted' type='checkbox' checked={muted} onChange={handleToggleMuted} />
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <label htmlFor='loop'>Loop</label>
-            </th>
-            <td>
-              <input id='loop' type='checkbox' checked={loop} onChange={handleToggleLoop} />
-            </td>
-          </tr>
-          <tr>
-            <th>Played</th>
-            <td><progress max={1} value={played} /></td>
-          </tr>
-          <tr>
-            <th>Loaded</th>
-            <td><progress max={1} value={loaded} /></td>
-          </tr>
-          <tr>
-            <th>duration</th>
-            <td><Duration seconds={duration} /></td>
-          </tr>
-          <tr>
-            <th>elapsed</th>
-            <td><Duration seconds={duration * played} /></td>
-          </tr>
-          <tr>
-            <th>remaining</th>
-            <td><Duration seconds={duration * (1 - played)} /></td>
-          </tr>
-        </tbody>
-      </table>
-    </Fragment>
+        <table>
+          <tbody>
+            <tr>
+              <th>Controls</th>
+              <td>
+                <Button onClick={handleStop}>Stop</Button>
+                <Button onClick={handlePlayPause}>{playing ? 'Pause' : 'Play'}</Button>
+              </td>
+            </tr>
+            <tr>
+              <th>Seek</th>
+              <td>
+                <input
+                  type='range' min={0} max={0.999999} step='any'
+                  value={played}
+                  onMouseDown={handleSeekMouseDown}
+                  onChange={handleSeekChange}
+                  onMouseUp={handleSeekMouseUp}
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>Volume</th>
+              <td>
+                <input type='range' min={0} max={1} step='any' value={volume} onChange={handleVolumeChange} />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor='muted'>Muted</label>
+              </th>
+              <td>
+                <input id='muted' type='checkbox' checked={muted} onChange={handleToggleMuted} />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor='loop'>Loop</label>
+              </th>
+              <td>
+                <input id='loop' type='checkbox' checked={loop} onChange={handleToggleLoop} />
+              </td>
+            </tr>
+            <tr>
+              <th>Played</th>
+              <td><progress max={1} value={played} /></td>
+            </tr>
+            <tr>
+              <th>Loaded</th>
+              <td><progress max={1} value={loaded} /></td>
+            </tr>
+            <tr>
+              <th>duration</th>
+              <td><Duration seconds={duration} /></td>
+            </tr>
+            <tr>
+              <th>elapsed</th>
+              <td><Duration seconds={duration * played} /></td>
+            </tr>
+            <tr>
+              <th>remaining</th>
+              <td><Duration seconds={duration * (1 - played)} /></td>
+            </tr>
+          </tbody>
+        </table>
+      </Grid>
+    </Container>
   );
 }
 
